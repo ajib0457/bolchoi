a04346b098081e9691c8b86c1dcf8a795d6f0ff6
Simulations analysis <35188545+ajib0457@users.noreply.github.com>
2018-01-08 16:58:58 +1100

    Major update to post_hess
    I have now updated this code so that it creates a mask which indicates the type of LSS (void,sheet, filament or cluster) for each pixel. In addition, I have included eigvecs for the filament and the sheet, so make sure to use the mask to filter out a specific set of eigenvectors from the corresponding array

--- Artemis/correl/my_den/post_hess/post_hess0.py
+++ Artemis/correl/my_den/post_hess/post_hess0.py
@@ -4,6 +4,7 @@ import pickle
 from matplotlib import cm
 import pandas as pd
 import h5py
+import sklearn.preprocessing as skl
 #I should make 5 codes to calculate
 #225GB+
 part=0#Change for each file. START FROM 0
@@ -69,54 +70,113 @@ del eigvals
 #link eigenvalues as keys to eigenvectors as values inside dictionary    
 vec_arr_num,vec_row,vec_col=np.shape(eigvecs)
 values=np.reshape(eigvecs.transpose(0,2,1),(vec_row*vec_arr_num,vec_col))#orient eigenvectors so that each row is an eigenvector
+values=skl.normalize(values)
 del eigvecs
 eigvals_unsorted=eigvals_unsorted.flatten()
-vecsvals=np.column_stack((eigvals_unsorted,values))
-del eigvals_unsorted
-del values
 
-####Classifier#### 
-recon_img=np.zeros([grid_nodes_true**3])
-#NEW FILAMENT FILTER#
-recon_filt_one=np.where(eig_three<0)
-recon_filt_two=np.where(eig_two<0)
-recon_filt_three=np.where(eig_one>=0)
+
+lss=['void','sheet','filament','cluster']#Choose which LSS you would like to get classified
+def lss_classifier(lss,eigvals_unsorted,values,eig_one,eig_two,eig_three):
+    
+    ####Classifier#### 
+    '''
+    This is the classifier function which takes input:
+    
+    lss: the labels of Large scale structure which will be identified pixel by pixel and also eigenvectors 
+    will be retrieved if applicable.
+    
+    vecsvals: These are the eigenvalues and eigevector pairs which correspond row by row.
+    
+    eig_one,two and three: These are the isolated eigenvalues 
+    
+    This function will output:
+    
+    eig_fnl: An array containing all of the relevent eigenvectors for each LSS type
+    
+    mask_fnl: array prescribing 0-void, 1-sheet, 2-filament and 3-cluster
+    
+    '''
+    eig_fnl=np.zeros((grid_nodes_true**3,4))
+    mask_fnl=np.zeros((grid_nodes_true**3))
+    for i in lss:
+        vecsvals=np.column_stack((eigvals_unsorted,values))
+        recon_img=np.zeros([grid_nodes_true**3])
+        if (i=='void'):
+            recon_filt_one=np.where(eig_three>0)
+            recon_filt_two=np.where(eig_two>0)
+            recon_filt_three=np.where(eig_one>0)
+        if (i=='sheet'):
+            recon_filt_one=np.where(eig_three<0)
+            recon_filt_two=np.where(eig_two>=0)
+            recon_filt_three=np.where(eig_one>=0)
+        if (i=='filament'):
+            recon_filt_one=np.where(eig_three<0)
+            recon_filt_two=np.where(eig_two<0)
+            recon_filt_three=np.where(eig_one>=0)
+        if (i=='cluster'):
+            recon_filt_one=np.where(eig_three<0)
+            recon_filt_two=np.where(eig_two<0)
+            recon_filt_three=np.where(eig_one<0)
+        
+        #LSS FILTER#
+        
+        recon_img[recon_filt_one]=1
+        recon_img[recon_filt_two]=recon_img[recon_filt_two]+1
+        recon_img[recon_filt_three]=recon_img[recon_filt_three]+1  
+        del recon_filt_one
+        del recon_filt_two
+        del recon_filt_three
+        recon_img=recon_img.flatten()
+        recon_img=recon_img.astype(np.int8)
+        mask=(recon_img !=3)#Up to this point, a mask is created to identify where there are NO filaments...
+        mask_true=(recon_img ==3)
+        del recon_img
+        vecsvals=np.reshape(vecsvals,(grid_nodes_true**3,3,4))
+        
+        
+        #Find relevent eigpairs
+        if (i=='void'):#There is no appropriate axis of a void?
+            mask_fnl[mask_true]=0
+            del mask_true
+            
+        if (i=='sheet'):
+            vecsvals[mask,:,:]=np.ones((3,4))*9#...which are then converted into -9 at this point
+            del mask
+            fnd_prs=np.where(vecsvals[:,:,0]<0)#find LSS axis
+            eig_fnl[fnd_prs[0],:]=vecsvals[fnd_prs[0],fnd_prs[1],:]
+            mask_fnl[mask_true]=1
+            del mask_true
+
+        if (i=='filament'):
+            vecsvals[mask,:,:]=np.ones((3,4))*-9#...which are then converted into -9 at this point
+            del mask
+            fnd_prs=np.where(vecsvals[:,:,0]>=0)#find LSS axis
+            eig_fnl[fnd_prs[0],:]=vecsvals[fnd_prs[0],fnd_prs[1],:]
+            mask_fnl[mask_true]=2
+            del mask_true
+            
+        if (i=='cluster'):#There is no appropriate axis of a void?
+            mask_fnl[mask_true]=3
+            del mask_true            
+        
+    return eig_fnl,mask_fnl  
+    
+eig_fnl,mask_fnl= lss_classifier(lss,eigvals_unsorted,values,eig_one,eig_two,eig_three)#Function run
 del eig_three
 del eig_two
-del eig_one
-recon_img[recon_filt_one]=1
-recon_img[recon_filt_two]=recon_img[recon_filt_two]+1
-recon_img[recon_filt_three]=recon_img[recon_filt_three]+1  
-del recon_filt_one
-del recon_filt_two
-recon_img=recon_img.flatten()
-recon_img=recon_img.astype(np.int8)
-mask=(recon_img !=3)
-del recon_img
-vecsvals=np.reshape(vecsvals,(grid_nodes_true**3,3,4))
-vecsvals[mask,:,:]=np.ones((3,4))*-9
-del mask
-#Find the empty pixels indices
-eig_fil_fnl=np.zeros((grid_nodes_true**3,4))#to compile after finding non filament rows and filament rows
-fnd_pxls=np.where(vecsvals==-9)
-fnd_pxls=np.unique(fnd_pxls[0])
-#Find eig 1 pairs
-fnd_prs=np.where(vecsvals[:,:,0]>=0)
-#Compile eig_fil_fnl
-eig_fil_fnl[fnd_pxls,0:]=9
-del fnd_pxls
-eig_fil_fnl[fnd_prs[0],:]=vecsvals[fnd_prs[0],fnd_prs[1],:]
-del fnd_prs
-del vecsvals
-
-#recon_vecs=eig_fil_fnl[:,0]
-recon_vecs_x=eig_fil_fnl[:,1]
-recon_vecs_y=eig_fil_fnl[:,2]
-recon_vecs_z=eig_fil_fnl[:,3]
-del eig_fil_fnl
+del eig_one  
+del eigvals_unsorted
+del values  
+recon_vecs_x=eig_fnl[:,1]
+recon_vecs_y=eig_fnl[:,2]
+recon_vecs_z=eig_fnl[:,3]
 
 f=h5py.File("/scratch/GAMNSCM2/bolchoi_z0/correl/my_den/files/output_files/eigvecs/fil_recon_vecs_DTFE_gd%d_smth%sMpc_%d.h5" %(grid_nodes,round(std_dev_phys,3),part), 'w')
 f.create_dataset('/group%d/x'%part,data=recon_vecs_x)
 f.create_dataset('/group%d/y'%part,data=recon_vecs_y)
 f.create_dataset('/group%d/z'%part,data=recon_vecs_z)
 f.close()
+
+f2=h5py.File("/scratch/GAMNSCM2/bolchoi_z0/correl/my_den/files/output_files/eigvecs/fil_recon_vecs_DTFE_gd%d_smth%sMpc_%d_mask.h5" %(grid_nodes,round(std_dev_phys,3),part), 'w')
+f2.create_dataset('/mask%d'%part,data=mask_fnl)
+f2.close()
