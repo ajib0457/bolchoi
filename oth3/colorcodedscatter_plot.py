f7e870a95a1fd76312d9bc7a4b51246eaab3fdad
Simulations analysis <35188545+ajib0457@users.noreply.github.com>
2018-01-09 17:15:55 +1100

    Major update: color coded with size scale
    I have made major improvements to this code including: proper color coding where I control the colors willingly. Also scale the indicators by vradius or vmass by toggling the s=... index from 3 to 5 in line 57 (make sure to change line 67 label accordingly)

--- oth3/colorcodedscatter_plot.py
+++ oth3/colorcodedscatter_plot.py
@@ -1,6 +1,5 @@
 import numpy as np
 from matplotlib import pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
 import h5py
 import pandas as pd
 
@@ -15,21 +14,21 @@ grid_phys=1.*sim_sz/grid_den#Size of each voxel in physical units
 val_phys=1.*(2*fnl_val)/grid_den#Value in each grid voxel
 std_dev_phys=1.*s/val_phys*grid_phys
 
-f=h5py.File("/import/oth3/ajib0457/bolchoi_z0/investigation/bolchoi_halo_colordata_grid%s_smth%s_500part.h5"%(grid_den,round(std_dev_phys,2)), 'r')
+f=h5py.File("/import/oth3/ajib0457/bolchoi_z0/investigation/bolchoi_halo_colordata_grid%s_smth%s_500part_xyzm_id.h5"%(grid_den,round(std_dev_phys,2)), 'r')
 data_in=f['/group/x'][:]#already filtered for >=500
-
-mass = pd.read_csv('/import/oth3/ajib0457/bolchoi_z0/catalogs/rockstar/bolchoi_DTFE_rockstar_halos_z0_xyz_m_j',sep=r"\s+",lineterminator='\n', header = None)
-mass=mass.as_matrix()
 
 rad = pd.read_csv('/import/oth3/ajib0457/bolchoi_z0/catalogs/rockstar/bolchoi_DTFE_rockstar_halos500_m_r',sep=r"\s+",lineterminator='\n', header = None)
 rad=rad.as_matrix()#already filtered for >=500
 
-partcl_500=np.where((mass[:,3]/(1.35*10**8))>=500)#filter out halos with <500 particles
-mass=mass[partcl_500]
-data=np.column_stack((data_in,mass[:,3],rad[:,1]))
-#data=np.column_stack((data,rad))
+#scale mass and radii of halos
+data_in[:,3]=(data_in[:,3]-np.min(data_in[:,3]))/(np.max(data_in[:,3])-np.min(data_in[:,3]))
+#data_in[:,3]=data_in[:,3]/(np.sum(data_in[:,3]))
+rad[:,1]=(rad[:,1]-np.min(rad[:,1]))/(np.max(rad[:,1])-np.min(rad[:,1]))
+#rad[:,1]=rad[:,1]/(np.sum(rad[:,1]))
+data=np.column_stack((data_in,rad[:,1]))
+
 #slices options
-slc=10#30 slices per 0.1 length
+slc=425#30 slices per 0.1 length
 x,y,z=0,1,2#slice through which axis
 box=np.max(data[:,0])#subset box length
 partcl_thkns=5#Thickness of the particle slice, Mpc
@@ -45,20 +44,25 @@ for i in range(len(data)):
         #density field
         result_hals=data[i,:]
         partcls=np.row_stack((partcls,result_hals))
-
-#add 8 points, inside PARTICLES array, on vertices of cube to avoid plotting fisheyed effect
-vert_box=np.array([[box,0,0,0,0,0],[0,box,0,0,0,0],[0,0,box,0,0,0],[box,box,0,0,0,0],[box,box,box,0,0,0],[box,0,box,0,0,0],[0,box,box,0,0,0]])        
-partcls=np.row_stack((partcls,vert_box))
+partcls = np.delete(partcls, (0), axis=0)
 
 #Plotting
-fig=plt.figure(figsize=(50,50),dpi=300)
-ax=fig.add_subplot(111, projection='3d')
-#plt.title('particle Distribution')
-scale_mass=partcls[:,4]/sum(partcls[:,4])*100010
-vrad=partcls[:,5]**1.1
-ax.scatter(partcls[:,0],partcls[:,1],partcls[:,2],c=partcls[:,3]*2,cmap=plt.cm.autumn,s=100)
-ax.view_init(elev=0,azim=-90)#upon generating figure, usually have to rotate manually by 90 deg. clockwise 
-plt.xlabel('x') 
-plt.ylabel('y')
-plt.savefig('/import/oth3/ajib0457/bolchoi_z0/investigation/plots/color_slices/bolchoi_halocolor_gd%d_slc%d_thck%sMpc.png' %(grid_den,slc,partcl_thkns))
+fig, ax = plt.subplots(figsize=(15,15),dpi=400)
+
+i=0#initiate mask for plot loop
+for color in ['red', 'green', 'blue','yellow']:
+    lss_plt_filt=np.where(partcls[:,4]==i)
+    lss=['voids','sheets','filaments','clusters']  
+    scale_factor=1000
+    ax.scatter(partcls[lss_plt_filt,0],partcls[lss_plt_filt,2],s=partcls[lss_plt_filt,5]*scale_factor,c=color,label=lss[i],alpha=0.9, edgecolors='none')
+    i+=1
+
+#ax.view_init(elev=0,azim=-90)#upon generating figure, usually have to rotate manually by 90 deg. clockwise 
+plt.xlabel('x[Mpc/h]') 
+plt.ylabel('y[Mpc/h]')
+ax.legend()
+ax.grid(True)
+ax.set_xlim([0,250])
+ax.set_ylim([0,250])
+plt.savefig('/import/oth3/ajib0457/bolchoi_z0/investigation/plots/color_slices/bolchoi_halocolor_gd%d_slc%d_thck%sMpc_scl_vradius.png' %(grid_den,slc,partcl_thkns))
 f.close()
