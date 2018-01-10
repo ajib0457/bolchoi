f35226f5b4b7ba34fc01af5a4bc0352b7fa55f32
Simulations analysis <35188545+ajib0457@users.noreply.github.com>
2018-01-10 15:09:28 +1100

    Major changes
    I have replaced 3d plotting with 2d as it is a slice and this leaves the plot more presentable and paper quality.

--- Artemis/investigation/halo_slicer/haloslice_plotter.py
+++ Artemis/investigation/halo_slicer/haloslice_plotter.py
@@ -3,8 +3,6 @@ import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from sympy import *
-from mpl_toolkits.mplot3d import Axes3D
-import pickle
 import pandas as pd
 #Halos
 data = pd.read_csv('/scratch/GAMNSCM2/bolchoi_z0/cat_reconfig/files/output_files/bolchoi_DTFE_rockstar_halos_z0',sep=r"\s+",lineterminator='\n', header = None)
@@ -21,10 +19,10 @@ halos=np.column_stack((Xc,Yc,Zc))
 #plotting slice
         
 #slices options
-slc=10#30 slices per 0.1 length
+slc=300#30 slices per 0.1 length
 x,y,z=0,1,2#slice through which axis
 box=np.max(halos[:,0])#subset box length
-grid_den=2000#density field grid resolution
+grid_den=850#density field grid resolution
 partcl_thkns=1.3#Thickness of the particle slice, Mpc
 lo_lim_partcl=1.*slc/(grid_den)*box-1.*partcl_thkns/2 #For particle distribution
 hi_lim_partcl=lo_lim_partcl+partcl_thkns #For particle distributionn
@@ -38,17 +36,12 @@ for i in range(len(halos)):
         #density field
         result_hals=halos[i,:]
         partcls=np.row_stack((partcls,result_hals))
-
-#add 8 points, inside PARTICLES array, on vertices of cube to avoid plotting fisheyed effect
-vert_box=np.array([[box,0,0],[0,box,0],[0,0,box],[box,box,0],[box,box,box],[box,0,box],[0,box,box]])        
-partcls=np.row_stack((partcls,vert_box))
+partcls = np.delete(partcls, (0), axis=0)
 
 #Plotting
-fig=plt.figure(figsize=(40,40),dpi=150)
-ax=fig.add_subplot(111, projection='3d')
-#plt.title('particle Distribution')
-ax.scatter(partcls[:,0],partcls[:,1],partcls[:,2],c='r')
-ax.view_init(elev=0,azim=-90)#upon generating figure, usually have to rotate manually by 90 deg. clockwise 
-plt.xlabel('x') 
-plt.ylabel('y')
-plt.savefig('snpsht_012prtcls_VELOCIhalos_gd%d_slc%d_thck%sMpc.png' %(grid_den,slc,partcl_thkns))
+fig, ax = plt.subplots(figsize=(15,15),dpi=400)
+ax.scatter(partcls[:,0],partcls[:,2],c='r')
+plt.xlabel('x[Mpc/h]') 
+plt.ylabel('y[Mpc/h]')
+ax.grid(True)
+plt.savefig('/scratch/GAMNSCM2/bolchoi_z0/investigation/bolchoi_halosall_gd%d_slc%d_thck%sMpc.png' %(grid_den,slc,partcl_thkns))
