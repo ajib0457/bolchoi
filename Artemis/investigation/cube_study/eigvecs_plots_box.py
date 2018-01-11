b922ce5bc550c61c648ddae3ba17ff1bc99638e7
Simulations analysis <35188545+ajib0457@users.noreply.github.com>
2018-01-11 18:37:19 +1100

    Major change-slicer in this case, not dicer
    This slight variation of the code creates a slice instead of a cube, but this is easily reversible by getting rid of the code on lines 64-69 and on line 86 get rid of [lim_x_min:lim_x_max,sub_slc:post_slc,lim_z_min:lim_z_max] and dito for other lines.

--- Artemis/investigation/cube_study/eigvecs_plots_box.py
+++ Artemis/investigation/cube_study/eigvecs_plots_box.py
@@ -2,6 +2,7 @@ import numpy as np
 import pickle
 from mayavi import mlab
 import h5py
+import sklearn.preprocessing as skl
 
 #Initial data from density field and classification
 sim_sz=250#Mpc
@@ -52,47 +53,83 @@ halo_vecs_z=np.reshape(halo_vecs_z,(box_sz_x_max-box_sz_x_min,box_sz_y_max-box_s
 spin_res=open('/import/oth3/ajib0457/bolchoi_z0/investigation/my_denbox/resid_grid%s_%s_%s_%s_%s_%s_%s.pkl'%(grid_nodes,box_sz_x_min,box_sz_x_max,box_sz_y_min,box_sz_y_max,box_sz_z_min,box_sz_z_max),'rb')
 res=pickle.load(spin_res)
 '''
-#mask out all but filament eigenvectors
-mask_fil=np.zeros(np.shape(mask))
-mask_fil_indx=np.where(mask==2)
-mask_fil[mask_fil_indx]=1
-mask_fil=(mask_fil !=1)
-recon_vecs_x[mask_fil]=0
-recon_vecs_y[mask_fil]=0
-recon_vecs_z[mask_fil]=0
-'''
-#mask out all but sheet eigenvectors
-mask_sht=np.zeros(np.shape(mask))
-mask_sht_indx=np.where(mask==1)
-mask_sht[mask_sht_indx]=1
-mask_sht=(mask_sht !=1)
-recon_vecs_x[mask_sht]=0
-recon_vecs_y[mask_sht]=0
-recon_vecs_z[mask_sht]=0
-'''
-
+#new plotting idea
+x,y,z=np.shape(mask)
+fakevecs=skl.normalize(np.random.rand(x*y*z,3))
+#fakevecs=np.reshape(fakevecs,(x,y,z,3))#Fake vectors to create isosurfaces for each LSS
+fakevecs_x=np.reshape(fakevecs[:,0],(x,y,z))
+fakevecs_y=np.reshape(fakevecs[:,1],(x,y,z))
+fakevecs_z=np.reshape(fakevecs[:,2],(x,y,z))
 #3d eigenvector plot using mayavi
+sub_slc=2
+post_slc=3
+lim_x_min=0
+lim_x_max=850
+lim_z_min=0
+lim_z_max=850
+
 mlab.close(all=True)
 pltt=mlab.figure(size=(2000,2000))
 
-pltt=mlab.pipeline.vector_field(recon_vecs_x,recon_vecs_y,recon_vecs_z)#optional: vmin=0.6 and vmax=0.4
-#pltt_halos=mlab.pipeline.vector_field(halo_vecs_x,halo_vecs_y,halo_vecs_z)#optional: vmin=0.6 and vmax=0.4
-mlab.pipeline.vectors(pltt,scale_factor=3,mask_points=800,line_width=0.6)#mask_points=2 This is for the eigenvectors lSS
-#mlab.pipeline.vectors(pltt_halos,scale_factor=1,mask_points=3,line_width=0.6,color=(1,1,1))#This will be for the halo vectors.
-#pltt=mlab.pipeline.volume(mlab.pipeline.scalar_field(recon_img),vmin=0.1,vmax=0.9)#optional: vmin=0.6 and vmax=0.4
+manifest=['clusters','filaments','sheets','voids']
+for i in manifest:
+    
+    if i=='clusters':
+        #mask out all but sheet eigenvectors
+        recon_vecs_x_clus=np.zeros((x,y,z))
+        recon_vecs_y_clus=np.zeros((x,y,z))
+        recon_vecs_z_clus=np.zeros((x,y,z))
+        mask_indx=np.where(mask==3)
+        recon_vecs_x_clus[mask_indx]=fakevecs_x[mask_indx]
+        recon_vecs_y_clus[mask_indx]=fakevecs_y[mask_indx]
+        recon_vecs_z_clus[mask_indx]=fakevecs_z[mask_indx]        
+        pltt_clus=mlab.pipeline.vector_field(recon_vecs_x_clus[lim_x_min:lim_x_max,sub_slc:post_slc,lim_z_min:lim_z_max],recon_vecs_y_clus[lim_x_min:lim_x_max,sub_slc:post_slc,lim_z_min:lim_z_max],recon_vecs_z_clus[lim_x_min:lim_x_max,sub_slc:post_slc,lim_z_min:lim_z_max])#optional: vmin=0.6 and vmax=0.4
+        mlab.pipeline.vectors(pltt_clus,scale_factor=3,mask_points=1,line_width=0.6,color=(1,0,0))#mask_points=2 This is for the eigenvectors lSS
+        magnitude_clus = mlab.pipeline.extract_vector_norm(pltt_clus) #this line aids in the following line to create isosurfaces.
+        mlab.pipeline.iso_surface(magnitude_clus, contours=[1.9, 0.5],color=(1,0,0))#this is the isosurfaces plotter. You can simply delete these two lines to plot a vector field
+        
+    if i=='filaments':
+        #mask out all but filament eigenvectors
+        recon_vecs_x_fil=np.zeros((x,y,z))
+        recon_vecs_y_fil=np.zeros((x,y,z))
+        recon_vecs_z_fil=np.zeros((x,y,z))
+        mask_indx=np.where(mask==2)
+        recon_vecs_x_fil[mask_indx]=recon_vecs_x[mask_indx]
+        recon_vecs_y_fil[mask_indx]=recon_vecs_y[mask_indx]
+        recon_vecs_z_fil[mask_indx]=recon_vecs_z[mask_indx]
+        pltt_fil=mlab.pipeline.vector_field(recon_vecs_x_fil[lim_x_min:lim_x_max,sub_slc:post_slc,lim_z_min:lim_z_max],recon_vecs_y_fil[lim_x_min:lim_x_max,sub_slc:post_slc,lim_z_min:lim_z_max],recon_vecs_z_fil[lim_x_min:lim_x_max,sub_slc:post_slc,lim_z_min:lim_z_max])#optional: vmin=0.6 and vmax=0.4
+        mlab.pipeline.vectors(pltt_fil,scale_factor=5,mask_points=20,line_width=0.6,color=(1,1,0))#mask_points=2 This is for the eigenvectors lSS
+        magnitude_fil = mlab.pipeline.extract_vector_norm(pltt_fil) #this line aids in the following line to create isosurfaces.
+        mlab.pipeline.iso_surface(magnitude_fil, contours=[1.9, 0.5],color=(1,1,0))#this is the isosurfaces plotter. You can simply delete these two lines to plot a vector field
+       
+    if i=='sheets':
+        #mask out all but sheet eigenvectors
+        recon_vecs_x_sht=np.zeros((x,y,z))
+        recon_vecs_y_sht=np.zeros((x,y,z))
+        recon_vecs_z_sht=np.zeros((x,y,z))
+        mask_indx=np.where(mask==1)
+        recon_vecs_x_sht[mask_indx]=recon_vecs_x[mask_indx]
+        recon_vecs_y_sht[mask_indx]=recon_vecs_y[mask_indx]
+        recon_vecs_z_sht[mask_indx]=recon_vecs_z[mask_indx]
+        pltt_sheet=mlab.pipeline.vector_field(recon_vecs_x_sht[lim_x_min:lim_x_max,sub_slc:post_slc,lim_z_min:lim_z_max],recon_vecs_y_sht[lim_x_min:lim_x_max,sub_slc:post_slc,lim_z_min:lim_z_max],recon_vecs_z_sht[lim_x_min:lim_x_max,sub_slc:post_slc,lim_z_min:lim_z_max])#optional: vmin=0.6 and vmax=0.4
+        mlab.pipeline.vectors(pltt_sheet,scale_factor=5,mask_points=20,line_width=0.6,color=(0.2,0.2,0.9))#mask_points=2 This is for the eigenvectors lSS
+        magnitude_sht = mlab.pipeline.extract_vector_norm(pltt_sheet) #this line aids in the following line to create isosurfaces.
+        mlab.pipeline.iso_surface(magnitude_sht, contours=[1.9, 0.5],color=(0.2,0.2,0.9))#this is the isosurfaces plotter. You can simply delete these two lines to plot a vector field
+        
+    if i=='voids':
+        #mask out all but sheet eigenvectors
+        recon_vecs_x_vd=np.zeros((x,y,z))
+        recon_vecs_y_vd=np.zeros((x,y,z))
+        recon_vecs_z_vd=np.zeros((x,y,z))
+        mask_indx=np.where(mask==0)
+        recon_vecs_x_vd[mask_indx]=fakevecs_x[mask_indx]
+        recon_vecs_y_vd[mask_indx]=fakevecs_y[mask_indx]
+        recon_vecs_z_vd[mask_indx]=fakevecs_z[mask_indx]
+        pltt_vod=mlab.pipeline.vector_field(recon_vecs_x_vd[lim_x_min:lim_x_max,sub_slc:post_slc,lim_z_min:lim_z_max],recon_vecs_y_vd[lim_x_min:lim_x_max,sub_slc:post_slc,lim_z_min:lim_z_max],recon_vecs_z_vd[lim_x_min:lim_x_max,sub_slc:post_slc,lim_z_min:lim_z_max])#optional: vmin=0.6 and vmax=0.4
+        mlab.pipeline.vectors(pltt_vod,scale_factor=5,mask_points=20,line_width=0.6,color=(0.06666, 0.06666, 0.1804))#mask_points=2 This is for the eigenvectors lSS
+        magnitude_vod = mlab.pipeline.extract_vector_norm(pltt_vod) #this line aids in the following line to create isosurfaces.
+        mlab.pipeline.iso_surface(magnitude_vod, contours=[1.9, 0.5],color=(0.06666, 0.06666, 0.1804),name='void')#this is the isosurfaces plotter. You can simply delete these two lines to plot a vector field
 
-del recon_vecs_x
-del recon_vecs_y
-del recon_vecs_z
-
-magnitude = mlab.pipeline.extract_vector_norm(pltt) #this line aids in the following line to create isosurfaces.
-mlab.pipeline.iso_surface(magnitude, contours=[1.9, 0.5])#this is the isosurfaces plotter. You can simply delete these two lines to plot a vector field
-
-#magnitude_h = mlab.pipeline.extract_vector_norm(pltt_halos) #this line aids in the following line to create isosurfaces.
-#mlab.pipeline.iso_surface(magnitude_h, contours=[1.9, 0.5])#this is the isosurfaces plotter. You can simply delete these two lines to plot a vector field
-
-#mlab.colorbar(nb_colors=4,nb_labels=0,orientation='vertical')#nb_labels=2,nb_colors=2 to have discrete colors
-#mlab.colorbar(orientation='vertical')
-mlab.axes()
-v=mlab.view(azimuth=-90,elevation=0)#this creates a variable, v, to which mayavi saves the azimuth & elevation angle to
-#mlab.savefig('DTFE_gd.png',size=(4000,4000))
+mlab.axes(z_axis_visibility=False,ranges=[0,250,0,250,0,250],xlabel='x[Mpc/h]',zlabel='y[Mpc/h]')
+v=mlab.view(azimuth=270.1,elevation=90,distance=2000)#this creates a variable, v, to which mayavi saves the azimuth & elevation angle to
+mlab.savefig('/import/oth3/ajib0457/bolchoi_z0/investigation/my_denbox/eigvecs/plots_eigvecs/4slc_gd850_gd.png')
