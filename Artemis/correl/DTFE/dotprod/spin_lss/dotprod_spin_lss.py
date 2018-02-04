60f79e92462a55c2a02191eb65230bb815c36b86
Simulations analysis <35188545+ajib0457@users.noreply.github.com>
2018-02-04 15:23:39 +1100

    some changes with saving

--- Artemis/correl/DTFE/dotprod/spin_lss/dotprod_spin_lss.py
+++ Artemis/correl/DTFE/dotprod/spin_lss/dotprod_spin_lss.py
@@ -1,11 +1,5 @@
 import numpy as np
-import matplotlib
-matplotlib.use('Agg')
-import matplotlib.pyplot as plt
-from matplotlib import cm
 import math as mth
-import pandas as pd
-import pickle
 import sklearn.preprocessing as skl
 import h5py
 #Initial data from density field and classification
@@ -48,13 +42,14 @@ del recon_vecs_flt_unnorm
 # -----------------
 
 #HALOS ------------
-data = pd.read_csv('/scratch/GAMNSCM2/bolchoi_z0/cat_reconfig/files/output_files/bolchoi_DTFE_rockstar_halos_z0_xyz_m_j',sep=r"\s+",lineterminator='\n', header = None)
+f=h5py.File("/scratch/GAMNSCM2/bolchoi_z0/cat_reconfig/files/output_files/bolchoi_DTFE_rockstar_allhalos_xyz_vxyz_jxyz_m_r.h5", 'r')#xyz vxvyvz jxjyjz & Rmass & Rvir: Halo radius (kpc/h comoving).
+data=f['/halo'][:]
+f.close()
 
-data=data.as_matrix()
-partcl_500=np.where((data[:,3]/(1.35*10**8))>=500)#filter out halos with <500 particles
+partcl_500=np.where((data[:,9]/(1.35*10**8))>=500)#filter out halos with <500 particles
 data=data[partcl_500]
 
-halo_mass=data[:,3]
+halo_mass=data[:,9]
 log_halo_mass=np.log10(halo_mass)#convert into log(M)
 mass_intvl=(np.max(log_halo_mass)-np.min(log_halo_mass))/tot_mass_bins
 #del halo_mass
@@ -69,9 +64,9 @@ mass_mask[hiint]=mass_mask[hiint]+1
 mass_indx=np.where(mass_mask==2)
 
 #Angular momentum
-Lx=data[:,4]
-Ly=data[:,5]
-Lz=data[:,6]
+Lx=data[:,6]
+Ly=data[:,7]
+Lz=data[:,8]
 #Positions
 Xc=data[:,0]
 Xc=Xc[mass_indx]
@@ -82,13 +77,9 @@ Zc=Zc[mass_indx]
 del data
 #normalized angular momentum vectors v1
 halos_mom=np.column_stack((Lx,Ly,Lz))
-
 halos_mom=halos_mom[mass_indx]
 norm_halos_mom=skl.normalize(halos_mom)
-#del halos_mom 
 halos=np.column_stack((Xc,Yc,Zc,norm_halos_mom))
-
-#del norm_halos_mom
 # -----------------
 
 #pre-binning for Halos ----------
@@ -99,14 +90,6 @@ Yc_min=2.0000000000000002e-05
 Yc_max=249.99996999999999
 Zc_min=1.0000000000000001e-05
 Zc_max=249.99997999999999
-#EXTEMES >=500 PARTICLES
-#Xc_min=0.00134
-#Xc_max=249.99911
-#Yc_min=0.00080999999999999985
-#Yc_max=249.99973
-#Zc_min=0.00027
-#Zc_max=249.99954
-
 
 Xc_mult=grid_nodes/(Xc_max-Xc_min)
 Yc_mult=grid_nodes/(Yc_max-Yc_min)
@@ -116,9 +99,6 @@ Xc_minus=Xc_min*grid_nodes/(Xc_max-Xc_min)+0.0000001
 Yc_minus=Yc_min*grid_nodes/(Yc_max-Yc_min)+0.0000001
 Zc_minus=Zc_min*grid_nodes/(Zc_max-Zc_min)+0.0000001
 #--------------------------------
-
-#dot product details
-n_dot_prod=0
 
 #grid=np.zeros((grid_nodes,grid_nodes,grid_nodes))
 store_spin=[]
@@ -134,9 +114,8 @@ for i in range(len(Xc)):
 del recon_vecs_unnorm
 del recon_vecs
 del halos
-store_spin=np.asarray(store_spin)      
+store_spin=np.asarray(store_spin) 
      
-spin_array=open('/project/GAMNSCM2/bolchoi_z0/correl/DTFE/files/output_files/dotproduct/spin_lss/DTFE_grid%d_spin_store_%dbins_fil_Log%s-%s_smth%sMpc_%sbins.pkl'%(grid_nodes,n_dot_prod,round(low_int_mass,2),round(hi_int_mass,2),round(std_dev_phys,3),tot_mass_bins),'wb')
-#spin_array=open('/project/GAMNSCM2/snapshot_012_LSS_class/correl/DTFE/files/output_files/dotproduct/spin_store_%dbins_fnl.pkl'%n_dot_prod,'wb')
-pickle.dump(store_spin,spin_array)
-spin_array.close()
+f=h5py.File('/project/GAMNSCM2/bolchoi_z0/correl/DTFE/files/output_files/dotproduct/spin_lss/DTFE_grid%d_spin_store_fil_Log%s-%s_smth%sMpc_%sbins.h5'%(grid_nodes,round(low_int_mass,2),round(hi_int_mass,2),round(std_dev_phys,3),tot_mass_bins),'w')     
+f.create_dataset('/dp',data=store_spin)
+f.close()
