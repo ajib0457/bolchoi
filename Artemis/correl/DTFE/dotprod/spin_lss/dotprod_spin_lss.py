0c429a78d3c8103bd00643f76a488ed8e45653d6
Simulations analysis <35188545+ajib0457@users.noreply.github.com>
2018-02-05 11:01:28 +1100

    Major change
    I have included the mask to do the filtering as before I was taking the dp of halos within filaments and sheets, now that I have the mask I can easily toggle which LSS I want to take the dp with.

--- Artemis/correl/DTFE/dotprod/spin_lss/dotprod_spin_lss.py
+++ Artemis/correl/DTFE/dotprod/spin_lss/dotprod_spin_lss.py
@@ -3,9 +3,9 @@ import math as mth
 import sklearn.preprocessing as skl
 import h5py
 #Initial data from density field and classification
-grid_nodes=1000
+grid_nodes=850
 tot_mass_bins=6
-mass_bin=5     # 0 to 5
+mass_bin=0     # 0 to 5
 sim_sz=250#Mpc
 in_val,fnl_val=-140,140
 tot_parts=8     #pertains to eigenvector files, not mass bins
@@ -18,7 +18,7 @@ std_dev_phys=1.*s/val_phys*grid_phys
 recon_vecs_x=np.zeros((grid_nodes**3))
 recon_vecs_y=np.zeros((grid_nodes**3))
 recon_vecs_z=np.zeros((grid_nodes**3))
-
+mask=np.zeros((grid_nodes**3))
 for part in range(tot_parts):
 #into 1 array
     nrows_in=int(1.*(grid_nodes**3)/tot_parts*part)
@@ -28,11 +28,15 @@ for part in range(tot_parts):
     recon_vecs_y[nrows_in:nrows_fn]=f['/group%d/y'%part][:]
     recon_vecs_z[nrows_in:nrows_fn]=f['/group%d/z'%part][:]
     f.close()
+    f2=h5py.File("/scratch/GAMNSCM2/bolchoi_z0/correl/my_den/files/output_files/eigvecs/fil_recon_vecs_DTFE_gd%d_smth%sMpc_%d_mask.h5" %(grid_nodes,round(std_dev_phys,3),part), 'r')
+    mask[nrows_in:nrows_fn]=f2['/mask%d'%part][:]
+    f2.close()
 
 recon_vecs_flt_unnorm=np.column_stack((recon_vecs_x,recon_vecs_y,recon_vecs_z))
 del recon_vecs_x
 del recon_vecs_y
 del recon_vecs_z
+mask=np.reshape(mask,(grid_nodes,grid_nodes,grid_nodes,3))#Three for the 3 vector components
 
 recon_vecs_flt_norm=skl.normalize(recon_vecs_flt_unnorm)#I should not normalize becauase they are already normalized and also the classifier (9) mask will be ruined
 recon_vecs=np.reshape(recon_vecs_flt_norm,(grid_nodes,grid_nodes,grid_nodes,3))#Three for the 3 vector components
@@ -108,7 +112,7 @@ for i in range(len(Xc)):
     grid_index_y=mth.trunc(halos[i,1]*Yc_mult-Yc_minus) 
     grid_index_z=mth.trunc(halos[i,2]*Zc_mult-Zc_minus) 
     #calculate dot product and bin
-    if (recon_vecs_unnorm[grid_index_x,grid_index_y,grid_index_z,0]!=9):#condition includes recon_vecs_unnorm so that I may normalize the vectors which are being processed
+    if (mask[grid_index_x,grid_index_y,grid_index_z]==2):#condition includes recon_vecs_unnorm so that I may normalize the vectors which are being processed
         spin_dot=np.inner(halos[i,3:6],recon_vecs[grid_index_x,grid_index_y,grid_index_z,:]) 
         store_spin.append(spin_dot)
 del recon_vecs_unnorm
