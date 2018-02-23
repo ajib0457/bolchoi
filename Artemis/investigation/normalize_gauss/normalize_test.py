ea2bd4cd46b0dcb5a96b99e0e4c48aae01a4b7de
Simulations analysis <35188545+ajib0457@users.noreply.github.com>
2018-02-23 12:48:28 +1100

    major changes
    This code tests the 1D gaussian normalization using 4 methods, and tests 3D normalization of gaussian using 2 methods.

--- Artemis/investigation/normalize_gauss/normalize_test.py
+++ Artemis/investigation/normalize_gauss/normalize_test.py
@@ -2,34 +2,50 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.integrate import simps
 from numpy import trapz
-from mpl_toolkits.mplot3d import Axes3D
-
-
-s=3.92 #standard deviation of kernal
-gid_nodes=20
-kern_x,kern_y,kern_z=gid_nodes,gid_nodes,gid_nodes #kernel size
-in_val,fnl_val=-140,140  #value range of kernel 
-#Kernel generator
-X,Y,Z=np.meshgrid(np.linspace(in_val,fnl_val,kern_y),np.linspace(in_val,fnl_val,kern_x),np.linspace(in_val,fnl_val,kern_z))
-
-h=(1/np.sqrt(2*np.pi*s*s))**(3)*np.exp(-1/(2*s*s)*(Y**2+X**2+Z**2))
-fig = plt.figure(figsize=(40,40))
-ax = fig.add_subplot(111, projection='3d')
-trans=np.where(abs(h)>0)
-h[trans]=1
-ax.scatter(X,Y,Z,s=h)
-
+from sympy import *
 '''
-#a,stp=np.linspace(0,10,1000,retstep=True)
-#b=a*10
+#1D TEST FOR AREA----------------------
+#intial values
+in_val,fnl_val=-140,140
+s=2
+
+X=symbols('X')
+h=(1/sqrt(2*pi*s*s))**(1)*exp(-1/(2*s*s)*(X**2))
+h_int=integrate(h,(X,in_val,fnl_val))
+area = h_int
+print("area =", area)
+
+kern_x=850#kernel size
+X,stp=np.linspace(in_val,fnl_val,kern_x,retstep=True)
+h=(1/np.sqrt(2*np.pi*s*s))**(1)*np.exp(-1/(2*s*s)*(X**2))
+
 # Compute the area using the composite trapezoidal rule.
-area = trapz(dxx,X)#dx is the change between each value of a.
+area = trapz(h,X)#dx is the change between each value of a.
 print("area =", area)
 
 # Compute the area using the composite Simpson's rule.
-area = simps(dxx,dx=stp)
+area = simps(h,dx=stp)
 print("area =", area)
 
-area=np.sum(dxx)*stp
+area=np.sum(h)*stp
 print('area =', area)
+
+#compute the area using integration of function and then applying limits and sigma
+
 '''
+#3D TEST FOR VOLUME----------------------
+#initial values
+in_val,fnl_val=-140,140
+s=3.92
+
+X,Y,Z=symbols('X Y Z')
+h=(1/sqrt(2*pi*s*s))**(3)*exp(-1/(2*s*s)*(Y**2+X**2+Z**2))
+h_int=integrate(h,(X,in_val,fnl_val),(Y,in_val,fnl_val),(Z,in_val,fnl_val))
+print('volume = ',round(h_int,6))
+
+grid_nodes=300
+x,dxx=np.linspace(in_val,fnl_val,grid_nodes,retstep=True)
+X,Y,Z=np.meshgrid(x,x,x)
+h=(1/np.sqrt(2*np.pi*s*s))**(3)*np.exp(-1/(2*s*s)*(Y**2+X**2+Z**2))
+volume=np.sum(h)*dxx**3
+print('volume = ',round(volume,6))
